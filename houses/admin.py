3dc249817557ba79c470342c19917de7733e1fda
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-02-17 15:22:11 +0900

    \# 4.3 model and admin

--- houses/admin.py
+++ houses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import House
 
-# Register your models here.
+
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    list_display = ["name", "price_per_night", "address", "pets_allowed"]
+    list_filter = ["price_per_night", "pets_allowed"]
+    search_fields = ["address"]  # _startswith  텍스트로 시작하는 것만 검색
+    # search_fields 는 반드시 리스트 혹은 튜플이어야 한다. string이면 안된다.
--- houses/models.py
+++ houses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class House(models.Model):
+
+    """Model Definition for Houses"""
+
+    name = models.CharField(max_length=140)
+    price_per_night = models.PositiveIntegerField()  # 양수이면서 정수
+    description = models.TextField()
+    address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
