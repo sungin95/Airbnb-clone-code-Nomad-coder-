7aa623d92b0bd461cd87674a7275fbeb0d55e1eb
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-02-20 22:03:32 +0900

    4.4 models, admin fields - models에서 필드의 옵션을 조절하여 어드민 조절 - verbose_name(as와 비슷한 역할), help_text(부연 설명) - admin에서 admin페이지 어떻게 보여줄지 쉽게 관리 가능 - fields, exclude, list_display_links

--- houses/admin.py
+++ houses/admin.py
@@ -4,7 +4,13 @@ from .models import House
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
+    fields = (
+        "name",
+        "address",
+        ("price_per_night", "pets_allowed"),
+    )
     list_display = ["name", "price_per_night", "address", "pets_allowed"]
     list_filter = ["price_per_night", "pets_allowed"]
     search_fields = ["address"]  # _startswith  텍스트로 시작하는 것만 검색
     # search_fields 는 반드시 리스트 혹은 튜플이어야 한다. string이면 안된다.
+    list_display_links = ("name", "address")
