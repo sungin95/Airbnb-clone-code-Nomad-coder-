55d8517958212df035b95270d92deef37bc15d92
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-03-10 00:47:38 +0900

    5.3 models defaults 관하여 makemigrations error - 항상 1번을 선택해서 각각 넣어서 문제를 해결했는데. defaults를 넣어주는 방법도 있다.

--- users/models.py
+++ users/models.py
@@ -5,5 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = models.CharField(max_length=150, editable=False)
     last_name = models.CharField(max_length=150, editable=False)
-    name = models.CharField(max_length=150)
-    is_host = models.BooleanField()
+    name = models.CharField(max_length=150, default="")
+    is_host = models.BooleanField(
+        default=False
+    )  # True or False 둘만 있을 수 있는데 방법은 둘 default=False 혹은 null=True(없어도 된다.)
