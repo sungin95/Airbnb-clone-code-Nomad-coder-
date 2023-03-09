ed058f614e5542c7378e3969f37f7f67b0be8126
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-03-10 00:15:26 +0900

    5.1 users 추가 Users모델 생성, admin등록

--- config/settings.py
+++ config/settings.py
@@ -29,16 +29,22 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+SYSTEM_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "houses.apps.HousesConfig",
 ]
+
+CUSTOM_APPS = [
+    "houses.apps.HousesConfig",
+    "users.apps.UsersConfig",
+]
+
+INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -122,3 +128,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Auth
+AUTH_USER_MODEL = "users.User"
