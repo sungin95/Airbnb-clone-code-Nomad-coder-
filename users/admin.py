94ed8ef7c71063b78ccf9e70c8954fe1ec735193
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-03-10 01:13:40 +0900

    5.4 Custom Admin fieldsets - 자세한 내용은 공식 문서 참고

--- users/admin.py
+++ users/admin.py
@@ -5,4 +5,36 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    # editable=False로 처리 된것을 여기서도 처리해야 admin에서 문제가 안생긴다.
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": ("username", "password", "name", "email", "is_host"),
+                # "classes": ("wide",),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ("last_login", "date_joined"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+    # fieldsets = None
+    # fields = ("email", "password", "name")
+    list_display = ("username", "email", "name", "is_host")
