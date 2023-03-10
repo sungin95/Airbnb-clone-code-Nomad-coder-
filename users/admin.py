61577c0d6a7a7145ceb519040c72486845a3ac62
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-03-10 11:31:33 +0900

    6.0 models choices - class를 만들어서 models.TextChoices를 만듬.

--- users/admin.py
+++ users/admin.py
@@ -10,7 +10,17 @@ class CustomUserAdmin(UserAdmin):
         (
             "Profile",
             {
-                "fields": ("username", "password", "name", "email", "is_host"),
+                "fields": (
+                    "avatar",
+                    "username",
+                    "password",
+                    "name",
+                    "email",
+                    "is_host",
+                    "gender",
+                    "language",
+                    "curreny",
+                ),
                 # "classes": ("wide",),
             },
         ),
--- users/models.py
+++ users/models.py
@@ -3,9 +3,43 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=150, editable=False)
-    last_name = models.CharField(max_length=150, editable=False)
-    name = models.CharField(max_length=150, default="")
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")  # 첫번째는 value, 두번쨰는 관리자 페이지에서 보게되는 label
+        FEMALE = ("femaale", "Female")
+
+    class LanguageChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+
+    class CurrencyChoices(models.TextChoices):
+        WON = "won", "Korean Won"
+        USD = "usd", "Dollar"
+
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    avatar = models.ImageField(blank=True)  # poetry add Pillow
+    name = models.CharField(
+        max_length=150,
+        default="",
+    )
     is_host = models.BooleanField(
-        default=False
+        default=False,
     )  # True or False 둘만 있을 수 있는데 방법은 둘 default=False 혹은 null=True(없어도 된다.)
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
+    )
+    language = models.CharField(
+        max_length=2,
+        choices=LanguageChoices.choices,
+    )
+    curreny = models.CharField(
+        max_length=5,
+        choices=CurrencyChoices.choices,
+    )
