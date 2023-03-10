ee49044e33364f429b28ae332294591ed812e917
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-03-10 23:59:44 +0900

    6.7 reviews

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ CUSTOM_APPS = [
     "common.apps.CommonConfig",
     "experiences.apps.ExperiencesConfig",
     "categories.apps.CategoriesConfig",
+    "reviews.apps.ReviewsConfig",
 ]
 
 INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS
