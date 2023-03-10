b5199ce335163483f000e150dfab157854f96bf3
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-03-10 14:57:50 +0900

    6.4 room admin , 이름 보이는거 선택하기

--- rooms/admin.py
+++ rooms/admin.py
@@ -4,9 +4,30 @@ from .models import Room, Amenity
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "name",
+        "price",
+        "kind",
+        "owner",
+    )
+    list_filter = (
+        "country",
+        "city",
+        "pet_friendly",
+        "kind",
+        "amenities",
+    )
 
 
 @admin.register(Amenity)
 class AmenityAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "name",
+        "description",
+        "created_at",
+        "updated_at",
+    )
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
