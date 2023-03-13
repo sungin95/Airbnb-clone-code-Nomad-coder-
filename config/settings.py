1ab6e94dda8cc052a93f2044977c59ce76724fa9
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-03-13 11:30:38 +0900

    10.0 rest_framework install

--- config/settings.py
+++ config/settings.py
@@ -27,8 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
+
 SYSTEM_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -51,7 +54,7 @@ CUSTOM_APPS = [
     "direct_messages.apps.DirectMessagesConfig",
 ]
 
-INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS
+INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
 
 MIDDLEWARE = [
---
+++ 강의 정리/#10 REST Framework.md
@@ -0,0 +1,10 @@
+## 0
+
+`poetry add djangorestframework`
+
+settings 에서 rest_framework 를 추가해 주고
+
+INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS + CUSTOM_APPS 
+
+순서를 바꾸어 주자
+
