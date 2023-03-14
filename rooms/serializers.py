17a0c891a0d7649204449aabb235cc1e2489b1a7
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-03-14 20:33:52 +0900

    11.3 Rooms 관계성 확장 depth = 1 - 저렇게 하면 커스텀을 할 수 없다. - 다음시간에 선택적 관계확장하는 방법을 배울 것이다.

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Amenity
+from .models import Amenity, Room
+
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = "__all__"
+        depth = 1  # 관계가 있는 id의 정보도 보인다.
 
 
 class AmenitySerializer(ModelSerializer):
--- rooms/views.py
+++ rooms/views.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework.status import HTTP_204_NO_CONTENT
-from .models import Amenity
-from .serializers import AmenitySerializer
+from .models import Amenity, Room
+from .serializers import AmenitySerializer, RoomSerializer
 
 
 # /api/v1/rooms/amenities
@@ -56,38 +56,8 @@ class AmenityDetail(APIView):
         return Response(status=HTTP_204_NO_CONTENT)
 
 
-# from django.shortcuts import render
-# from django.http import HttpResponse
-# from .models import Room
-
-
-# def see_all_rooms(request):
-#     rooms = Room.objects.all()
-#     return render(
-#         request,
-#         "all_rooms.html",
-#         {
-#             "rooms": rooms,
-#             "title": "Hello! this title from django!",
-#         },
-#     )
-
-
-# def see_one_room(request, room_id):
-#     try:
-#         room = Room.objects.get(pk=room_id)
-#         return render(
-#             request,
-#             "room_detail.html",
-#             {
-#                 "room": room,
-#             },
-#         )
-#     except Room.DoesNotExist:
-#         return render(
-#             request,
-#             "room_detail.html",
-#             {
-#                 "not_found": True,
-#             },
-#         )
+class Rooms(APIView):
+    def get(self, request):
+        all_rooms = Room.objects.all()
+        serializer = RoomSerializer(all_rooms, many=True)
+        return Response(serializer.data)
