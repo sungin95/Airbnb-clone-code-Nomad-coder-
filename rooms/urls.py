ee9d0adad94d2e6717f117958eec5019f0f37122
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-03-14 15:51:51 +0900

    11.0 Amenities get post

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.see_all_rooms),
-    path("<int:room_id>/", views.see_one_room),
+    path("amenities/", views.Amenities.as_view()),
+    path("amenities/<int:pk>/", views.AmenityDetail.as_view()),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,35 +1,71 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Amenity
+from .serializers import AmenitySerializer
 
 
-def see_all_rooms(request):
-    rooms = Room.objects.all()
-    return render(
-        request,
-        "all_rooms.html",
-        {
-            "rooms": rooms,
-            "title": "Hello! this title from django!",
-        },
-    )
+# /api/v1/rooms/amenities
+class Amenities(APIView):
+    def get(self, request):
+        all_amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(all_amenities, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AmenitySerializer(data=request.data)
+        if serializer.is_valid():
+            amenity = serializer.save()
+            return Response(
+                AmenitySerializer(amenity).data,
+            )
+        else:
+            return Response(serializer.errors)
 
 
-def see_one_room(request, room_id):
-    try:
-        room = Room.objects.get(pk=room_id)
-        return render(
-            request,
-            "room_detail.html",
-            {
-                "room": room,
-            },
-        )
-    except Room.DoesNotExist:
-        return render(
-            request,
-            "room_detail.html",
-            {
-                "not_found": True,
-            },
-        )
+# /api/v1/rooms/amenities/1
+class AmenityDetail(APIView):
+    def get(self, request, pk):
+        pass
+
+    def put(self, request, pk):
+        pass
+
+    def delete(self, request, pk):
+        pass
+
+
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# from .models import Room
+
+
+# def see_all_rooms(request):
+#     rooms = Room.objects.all()
+#     return render(
+#         request,
+#         "all_rooms.html",
+#         {
+#             "rooms": rooms,
+#             "title": "Hello! this title from django!",
+#         },
+#     )
+
+
+# def see_one_room(request, room_id):
+#     try:
+#         room = Room.objects.get(pk=room_id)
+#         return render(
+#             request,
+#             "room_detail.html",
+#             {
+#                 "room": room,
+#             },
+#         )
+#     except Room.DoesNotExist:
+#         return render(
+#             request,
+#             "room_detail.html",
+#             {
+#                 "not_found": True,
+#             },
+#         )
