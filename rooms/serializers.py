07eb401ab373ab51986bc1cf49ebf56718d70f42
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-03-14 22:50:52 +0900

    11.6 Room Owner .save(Owner=request.user) - 관계형 데이터 모두 read_only

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -14,11 +14,17 @@ class AmenitySerializer(ModelSerializer):
 
 
 class RoomDetailSerializer(ModelSerializer):
+    # owner의 특정 정보만 보내기 위해 설정, read_only를 통해 생성시 owner정보 요구X
     owner = TinyUserSerializer(
-        read_only=True
-    )  # owner의 특정 정보만 보내기 위해 설정, read_only를 통해 생성시 owner정보 요구X
-    amenities = AmenitySerializer(many=True)
-    category = CategorrySerializer()
+        read_only=True,
+    )
+    amenities = AmenitySerializer(
+        read_only=True,
+        many=True,
+    )
+    category = CategorrySerializer(
+        read_only=True,
+    )
 
     class Meta:
         model = Room
