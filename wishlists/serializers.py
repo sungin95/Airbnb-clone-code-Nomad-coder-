a4420e48cb217016a68a21f696d0ddfde374857e
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-03-16 12:23:08 +0900

    11.20 wishlist detail toggle - WishlistDetaiil에서는 유저를 확인을 위해 get_object에서 user를 넣었고 - WishlistToggle room을 토글하도록 만들었고 이걸 위해 2개의 pk, 2개의 get_list, get_room room이 있는지 없는지 체크를 위해 .filter(pk=room.pk).exists()활용

--- wishlists/serializers.py
+++ wishlists/serializers.py
@@ -12,6 +12,7 @@ class WishlistSerializer(ModelSerializer):
     class Meta:
         model = Wishlist
         fields = (
+            "pk",
             "name",
             "rooms",
         )
--- wishlists/urls.py
+++ wishlists/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Wishlists
+from .views import Wishlists, WishlstDetail, WishlistToggle
 
 urlpatterns = [
     path("", Wishlists.as_view()),
+    path("<int:pk>", WishlstDetail.as_view()),
+    path("<int:pk>/rooms/<int:room_pk>", WishlistToggle.as_view()),
 ]
