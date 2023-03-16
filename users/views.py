d9eaf6508955d4cbf2779462d0a8ee4a12d863c6
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-03-16 23:11:04 +0900

    12.1 Create User, password

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ParseError
 from .serializers import PrivateUserSerializer
 
 
@@ -22,6 +23,26 @@ class Me(APIView):
         )
         if serializer.is_valid():
             user = serializer.save()
+            serializer = PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        # password제외 시키는까 없이도 생성되네
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+        serializer = PrivateUserSerializer(
+            data=request.data,
+        )
+        if serializer.is_valid():
+            user = serializer.save()
+            # hash회 된 패스워드로 만들기 위해.
+            user.set_password(password)
+            user.save()
             serializer = PrivateUserSerializer(user)
             return Response(serializer.data)
         else:
