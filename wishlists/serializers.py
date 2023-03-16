e8d3a2470c2cf9966329f43cb25a95f54c336a74
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-03-16 10:45:12 +0900

    11.19 wishlists filter(user=request.user)을 통해 유저것만 보여줌

---
+++ wishlists/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Wishlist
+from rooms.serializers import RoomListSerializer
+
+
+class WishlistSerializer(ModelSerializer):
+    rooms = RoomListSerializer(
+        many=True,
+        read_only=True,
+    )
+
+    class Meta:
+        model = Wishlist
+        fields = (
+            "name",
+            "rooms",
+        )
