8fa12aa155ace8c649516c42a3d2e84add496e8b
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-03-17 19:14:03 +0900

    15.2 Recap - 인증은 views보다 먼저 일어난다. - 인증 방법은 내가 선택 할 수 있다. - 인증된 유저는 (user, None)의 튜플 형태로 값을 보내야 한다.

--- config/authentication.py
+++ config/authentication.py
@@ -9,11 +9,13 @@ class TrustMeBroAuthentication(BaseAuthentication):
     def authenticate(self, request):
         username = request.headers.get("Trust-Me")
         if not username:
+            # 시도 x
             return None
         try:
             user = User.objects.get(username=username)
             return (user, None)  # 이게 규칙
         except User.DoesNotExist:
+            # 시도 하지만 잘못된 정보.
             raise AuthenticationFailed(f"No user {username}")
 
         # # user를 찾아야 하니까 headers를 확인해 보자
