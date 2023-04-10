ff8834e0f037ae74a5a458c459184c11e4446950
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-04-10 11:08:32 +0900

    20.14 error인식 못하는 문제 해결, status보내줘야함

--- users/views.py
+++ users/views.py
@@ -110,7 +110,10 @@ class LogIn(APIView):
             login(request, user)
             return Response({"ok": "Welcome!"})
         else:
-            return Response({"error": "wrong password"})
+            return Response(
+                {"error": "wrong password"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
 
 class LogOut(APIView):
