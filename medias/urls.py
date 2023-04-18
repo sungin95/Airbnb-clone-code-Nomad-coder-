b83edcf59c72dbed4f12def358f14b79a696e114
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-04-18 14:06:23 +0900

    21.6 One Time Upload, 결제해야 작동

--- medias/urls.py
+++ medias/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import PhotoDetail
+from .views import PhotoDetail, GetUploadURL
 
 urlpatterns = [
+    # 따라만 쳐 놓음
+    path("photos/get-url", GetUploadURL.as_view()),
+    #
     path("photos/<int:pk>", PhotoDetail.as_view()),
 ]
--- medias/views.py
+++ medias/views.py
@@ -1,3 +1,5 @@
+import requests
+from django.conf import settings
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.status import HTTP_200_OK
@@ -23,3 +25,19 @@ class PhotoDetail(APIView):
             raise PermissionDenied
         photo.delete()
         return Response(status=HTTP_200_OK)
+
+
+# 만들어만 놓음
+class GetUploadURL(APIView):
+    def post(self, request):
+        pass
+        url = f"https://api.cloudflare.com/client/v4/accounts/{settings.CF_ID}/images/v2/direct_upload"
+        one_time_url = requests.post(
+            url,
+            headers={
+                "Authorization": f"Bearer {settings.CF_TOKEN}",
+            },
+        )
+        one_time_url = one_time_url.json()
+        result = one_time_url.get("result")
+        return Response({"uploadURL": result.get("uploadURL")})
