2a867f9df0197046df4722c484d2edbe57f62c25
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-04-20 14:21:56 +0900

    CORS_ALLOWED_ORIGINS, CSRF_TRUSTED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -189,12 +189,13 @@ REST_FRAMEWORK = {
     ]
 }
 
-
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
-
+if DEBUG:
+    CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
+    CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
+else:
+    CORS_ALLOWED_ORIGINS = ["https://airbnb-frontend-5dsr.onrender.com"]
+    CSRF_TRUSTED_ORIGINS = ["https://airbnb-frontend-5dsr.onrender.com"]
 CORS_ALLOW_CREDENTIALS = True
-
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
 
 # GH_SECRET = env("GH_SECRET")
 
