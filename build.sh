1c7be327931ee642eba4a00e0145490ef0f44d97
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-04-20 12:48:16 +0900

    pip again

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-pip install --upgrade pippip install --force-reinstall -U setuptools
+pip install --upgrade pip
+pip install --force-reinstall -U setuptools
